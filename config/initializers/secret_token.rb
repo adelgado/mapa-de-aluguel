844a36e3b9953abb977a5003c294a329210697c2
Alessandro Delgado <adelgado1313@gmail.com>
2014-02-13 23:33:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapaDeAluguel::Application.config.secret_key_base = '1f8faf7122345c983f6d748460e3c84d5d89bcc3b471fb490c643815446863b9b872f6540515df2e6dcd9c9bbc1bad6388ab0eedde47b8e930d96a292142c443'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapaDeAluguel::Application.config.session_store :cookie_store, key: '_mapa-de-aluguel_session'
