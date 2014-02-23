43351dc7ad30cab93941b36c19f21a37e5e625b5
Alessandro Delgado <adelgado1313@gmail.com>
2014-02-23 17:49:05 +0100

    Display cities from the database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @cities = City.all
   end
 
   def show
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+	def city_path city
+		city.slug
+	end
 end
