1de0eabd5208b3d76b7d3931e48032a415bcd936
Alessandro Delgado <adelgado1313@gmail.com>
2014-02-23 17:20:08 +0100

    Add areas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213225028) do
+ActiveRecord::Schema.define(version: 20140223161226) do
+
+  create_table "areas", force: true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "cities", force: true do |t|
     t.string   "name"
